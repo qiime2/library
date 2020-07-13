71da37755eeede1fb67108d8916a6cc9f71a4b16
Matthew Dillon <matthewrdillon@gmail.com>
2020-07-13 15:17:44 -0700

    squash

--- library/index/tasks.py
+++ library/index/tasks.py
@@ -5,7 +5,7 @@ from celery.utils.log import get_task_logger
 logger = get_task_logger(__name__)
 
 
-@task(name='debug')
+@task(name='index.debug')
 def debug(payload):
     logger.info('Debug: %r' % (payload, ))
     return payload
