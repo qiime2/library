2ca68b04dd99aaa4501993d2faae26a5b147697d
Matthew Dillon <matthewrdillon@gmail.com>
2021-07-22 09:55:35 -0700

    lint

--- library/packages/models.py
+++ library/packages/models.py
@@ -12,6 +12,7 @@ from django.db import models
 
 from library.utils.models import AuditModel
 
+
 class Package(AuditModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
