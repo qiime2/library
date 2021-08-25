59279f7ce7125c708c97530b3ed5425fbf554484
Matthew Dillon <matthewrdillon@gmail.com>
2021-08-25 16:19:42 -0700

    lint

--- library/packages/admin.py
+++ library/packages/admin.py
@@ -71,8 +71,8 @@ class DistroInline(admin.StackedInline):
 
 
 class PackageAdmin(admin.ModelAdmin):
-    list_display=('name', 'token', 'repository', 'created_at', 'updated_at')
-    fields=('name', 'token', 'repository', 'created_at', 'updated_at')
+    list_display = ('name', 'token', 'repository', 'created_at', 'updated_at')
+    fields = ('name', 'token', 'repository', 'created_at', 'updated_at')
     readonly_fields = ('token', 'created_at', 'updated_at')
     inlines = [PackageBuildInline, DistroInline]
     ordering = ('-updated_at',)
