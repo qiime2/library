f41aa7c616573404e98c5bc93086e06598c2050b
Oddant1 <oddant1@hotmail.com>
2024-06-20 16:59:52 -0700

    writeFileSync

--- src/lib/scripts/GetRepoInfo.js
+++ src/lib/scripts/GetRepoInfo.js
@@ -76,4 +76,4 @@ for (const repo of repos) {
     repo_infos[owner][repo_name] = repo_info;
 }
 
-fs.writeFile('/static/info.json', JSON.stringify(repo_infos));
+fs.writeFileSync('/static/info.json', JSON.stringify(repo_infos));
