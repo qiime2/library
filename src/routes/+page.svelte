635102b476fb0234b653bdcff1aaf6589be52ab7
Oddant1 <oddant1@hotmail.com>
2024-06-25 16:48:08 -0700

    Add sort button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     let sortCol = '';
-    let sortAscending = true;
+    let sortDescending = true;
 
     let date_fetched: string | undefined = undefined;
     let repo_infos: Array<Object> = [];
@@ -19,20 +19,20 @@
 
     function sortArray(sortBy: string) {
         if (sortBy === sortCol) {
-            sortAscending = !sortAscending;
+            sortDescending = !sortDescending;
         } else {
             sortCol = sortBy;
-            sortAscending = true;
+            sortDescending = true;
         }
 
         function compareElements(a: Object, b: Object) {
-            const A = a[sortBy];
-            const B = b[sortBy];
+            const A = a[sortBy as keyof Object];
+            const B = b[sortBy as keyof Object];
 
             if (A < B) {
-                return sortAscending === true ? -1 : 1;
+                return sortDescending === true ? 1 : -1;
             } else if (A > B) {
-                return sortAscending === true ? 1 : -1;
+                return sortDescending === true ? -1 : 1;
             }
 
             return 0;
@@ -53,7 +53,32 @@
             <tr>
                 <th><button on:click={() => sortArray('owner')}>Repo Owner</button></th>
                 <th><button on:click={() => sortArray('repo_name')}>Name</button></th>
-                <th><button on:click={() => sortArray('stars')}>Stars</button></th>
+                <th>Stars<button on:click={() => sortArray('stars')}>
+                    <svg fill="none"
+                        width="10"
+                        height="10"
+                    >
+                        {#if sortCol !== 'stars'}
+                            <path
+                                stroke-width="3"
+                                stroke="rgb(119, 119, 119)"
+                                d="M0 5L10 5"
+                            />
+                        {:else if sortDescending}
+                            <path
+                                stroke-width="3"
+                                stroke="rgb(119, 119, 119)"
+                                d="M0 3L5 8a0,2 0 0 1 1,1M5 8L10 3"
+                            />
+                        {:else}
+                           <path
+                                stroke-width="3"
+                                stroke="rgb(119, 119, 119)"
+                                d="M0 8L5 3a0,2 0 0 1 1,1M5 3L10 8"
+                            />
+                        {/if}
+                    </svg>
+                </button></th>
                 <th><button on:click={() => sortArray('commit_date')}>Commit Date</button></th>
                 <th><button on:click={() => sortArray('runs_status')}>Commit Status</button></th>
             </tr>
