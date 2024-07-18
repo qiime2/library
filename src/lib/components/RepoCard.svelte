69a8dffb498ef05be652ef4911d4f32a9ee19e89
Oddant1 <oddant1@hotmail.com>
2024-07-18 11:09:00 -0700

    Change the stars section

--- src/lib/components/RepoCard.svelte
+++ src/lib/components/RepoCard.svelte
@@ -8,7 +8,12 @@
 
 <div class='repo-card'>
     <a href="repo?owner={repo_overview['Repo Owner']}&repo_name={repo_overview['Repo Name']}">{repo_overview['Repo Owner']}/{repo_overview['Repo Name']}</a>
-    <div># Stars: {repo_overview['Stars']}</div>
+    <div>
+        <svg height="16" width="160">
+            <!-- Put some kinda svg star here -->
+            <text x="20" y="14">{repo_overview['Stars']}</text>
+        </svg>
+    </div>
     <div>Latest Commit Date: {repo_overview['Commit Date']}</div>
     <div>Latest Commit Status: {repo_overview['Commit Status']}</div>
     <div>NOTE: Not all distros are necessarily supported for all epochs</div>
