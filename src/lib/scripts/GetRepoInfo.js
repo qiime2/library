6c16df666c358d2de5027f2aa066425fa6c6a0eb
Oddant1 <oddant1@hotmail.com>
2024-08-19 11:42:34 -0700

    more

--- src/lib/scripts/GetRepoInfo.js
+++ src/lib/scripts/GetRepoInfo.js
@@ -25,7 +25,8 @@ const repo_list = await octokit.request(
 );
 
 const repos = yaml.load(repo_list);
-console.log(`${repos}`)
+console.log(repo_list)
+console.log(repos)
 
 const overview = {
   Repos: {},
