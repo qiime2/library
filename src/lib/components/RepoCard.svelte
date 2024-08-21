5471087f12fd0b3221de8d44facbbe2855b01891
Oddant1 <oddant1@hotmail.com>
2024-08-21 15:18:21 -0700

    Put things on two lines in the card

--- src/lib/components/RepoCard.svelte
+++ src/lib/components/RepoCard.svelte
@@ -35,29 +35,31 @@
         {repo_overview["Short Description"]}
     </p>
     <!-- TODO: This is a placeholder -->
-    <span class="font-bold">User Docs: </span>
-    <a href="https://docs.qiime2.org/2024.5/">https://docs.qiime2.org/2024.5/</a>
     <div class="container">
-        <div class="flex">
-            <div>
-                <svg height="24" width="40" xmlns="http://www.w3.org/2000/svg" stroke-width="1.5" stroke="currentColor">
-                    <path fill="none" stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                    <text stroke-width="1" x="25" y="18">{repo_overview["Stars"]}</text>
-                </svg>
-            </div>
-            <div>
-                <span class="font-bold">Last Commit:</span>
-                {formatDate(repo_overview["Commit Date"])}&nbsp;
-            </div>
-            <div>
-                <span class="font-bold">Build Status:</span>
-                <span class="{getStatusColor()} h-fit">
-                    {repo_overview["Build Status"]}
-                </span>
-            </div>
+        <div>
+            <span class="font-bold">User Docs: </span>
+            <a href="https://docs.qiime2.org/2024.5/">https://docs.qiime2.org/2024.5/</a>
         </div>
-        <div class="lg:mx-10" style="word-break: break-all"><span class="font-bold">Distros:</span><CutOffList list={repo_overview["Distros"]} collapseNumber={2} /></div>
+        <div style="word-break: break-all"><span class="font-bold">Distros:</span><CutOffList list={repo_overview["Distros"]} collapseNumber={2} /></div>
         <div style="word-break: break-all"><span class="font-bold">Epochs:</span><CutOffList list={repo_overview["Epochs"]} collapseNumber={2} /></div>
+    </div>
+    <div class="container">
+        <div>
+            <svg height="24" width="40" xmlns="http://www.w3.org/2000/svg" stroke-width="1.5" stroke="currentColor">
+                <path fill="none" stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                <text stroke-width="1" x="25" y="18">{repo_overview["Stars"]}</text>
+            </svg>
+        </div>
+        <div>
+            <span class="font-bold">Last Commit:</span>
+            {formatDate(repo_overview["Commit Date"])}&nbsp;
+        </div>
+        <div>
+            <span class="font-bold">Build Status:</span>
+            <span class="{getStatusColor()} h-fit">
+                {repo_overview["Build Status"]}
+            </span>
+        </div>
     </div>
 </div>
 
@@ -84,7 +86,7 @@
 
     .container {
         @apply lg:grid
-        lg:grid-cols-[42%_29%_29%]
+        lg:grid-cols-[33%_33%_33%]
     }
 
     h1 {
