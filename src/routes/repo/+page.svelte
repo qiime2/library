d8b979cf161dd0c41614b0129dfdbd2a247b3c84
Oddant1 <oddant1@hotmail.com>
2024-09-16 10:51:12 -0700

    Remove redundant qiime2 in name

--- src/routes/repo/+page.svelte
+++ src/routes/repo/+page.svelte
@@ -39,7 +39,7 @@
 
     function updateInstallInstructions(release: string) {
       console.log(release)
-      env_name = `qiime2-${release}`;
+      env_name = `${release}`;
       env_filepath = `https://raw.githubusercontent.com/${repo_info['Repo Owner']}/${repo_info['Repo Name']}/refs/heads/${repo_info['Branch']}/.qiime2/library/environments/${repo_info['Repo Name']}-${release}.yml`
     }
 </script>
