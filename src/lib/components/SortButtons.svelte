2d7949fe29a9bc2418d00c4780f639aa5d5b161c
Oddant1 <oddant1@hotmail.com>
2024-09-17 15:51:11 -0700

    Cleanup sort button highlighting

--- src/lib/components/SortButtons.svelte
+++ src/lib/components/SortButtons.svelte
@@ -18,7 +18,13 @@
         sort_descending = sort_values.sort_descending;
     });
 
-    const columns = ["Repo Owner", "Repo Name", "Stars", "Commit Date", "Build Status"];
+    const columns = [
+        "Repo Owner",
+        "Repo Name",
+        "Stars",
+        "Commit Date",
+        "Build Status",
+    ];
 
     function sortButton(this_col: string) {
         if (this_col === sort_col) {
@@ -27,53 +33,51 @@
             sort_descending = true;
         }
 
-        overview_store.filtered_overviews = sortOverviews(overview_store.filtered_overviews, this_col, sort_descending);
+        overview_store.filtered_overviews = sortOverviews(
+            overview_store.filtered_overviews,
+            this_col,
+            sort_descending,
+        );
 
         sort_info.set({
             sort_col: this_col,
-            sort_descending: sort_descending
+            sort_descending: sort_descending,
         });
 
         overview.set({
-            ...overview_store
+            ...overview_store,
         });
     }
 </script>
 
 <div id="buttons">
     {#each columns as column}
-        <div class="sortButton">
-            <button on:click={() => sortButton(column)}>
-                <div class="float-left">
-                    {column}
-                </div>
-                <svg fill="none"
-                    width="10"
-                    height="10"
-                    class="svg"
-                >
-                    {#if sort_col !== column}
-                        <path
-                            stroke-width="3"
-                            stroke="rgb(119, 119, 119)"
-                            d="M0 5L10 5"
-                        />
-                    {:else if sort_descending}
-                        <path
-                            stroke-width="3"
-                            stroke="rgb(119, 119, 119)"
-                            d="M0 3L5 8a0,2 0 0 1 1,1M5 8L10 3"
-                        />
-                    {:else}
-                        <path
-                            stroke-width="3"
-                            stroke="rgb(119, 119, 119)"
-                            d="M0 8L5 3a0,2 0 0 1 1,1M5 3L10 8"
-                        />
-                    {/if}
-                </svg>
-            </button>
-        </div>
+        <button class="sortButton" on:click={() => sortButton(column)}>
+            <div class="float-left">
+                {column}
+            </div>
+            <svg fill="none" width="10" height="10" class="svg">
+                {#if sort_col !== column}
+                    <path
+                        stroke-width="3"
+                        stroke="rgb(119, 119, 119)"
+                        d="M0 5L10 5"
+                    />
+                {:else if sort_descending}
+                    <path
+                        stroke-width="3"
+                        stroke="rgb(119, 119, 119)"
+                        d="M0 3L5 8a0,2 0 0 1 1,1M5 8L10 3"
+                    />
+                {:else}
+                    <path
+                        stroke-width="3"
+                        stroke="rgb(119, 119, 119)"
+                        d="M0 8L5 3a0,2 0 0 1 1,1M5 3L10 8"
+                    />
+                {/if}
+            </svg>
+        </button>
     {/each}
 </div>
 
@@ -84,7 +88,8 @@
 
     .sortButton {
         @apply flex
-        mx-2;
+        px-2
+        rounded;
     }
 
     .label {
