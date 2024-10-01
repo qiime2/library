a3af18b4ac7f81030d5e156fdfa3b4e97cdca158
Oddant1 <oddant1@hotmail.com>
2024-10-01 12:17:21 -0700

    Cleanup +page.ts

--- src/routes/plugin/[owner]/[plugin]/+page.ts
+++ src/routes/plugin/[owner]/[plugin]/+page.ts
@@ -1,10 +1,8 @@
-export async function load({ params }) {
+export async function load({ params, fetch }) {
     const response = await fetch(`/json/${params.owner}/${params.plugin}.json`);
     const repo_info = await response.json();
 
     return {
-        owner: params.owner,
-        plugin: params.plugin,
         repo_info: repo_info
     }
 }
