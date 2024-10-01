2da1ae84bb5c65bcb80da5492b65fab728f36524
Oddant1 <oddant1@hotmail.com>
2024-10-01 12:06:37 -0700

    Move things to page.ts

--- src/routes/plugin/[owner]/[plugin]/+page.ts
+++ src/routes/plugin/[owner]/[plugin]/+page.ts
@@ -1,6 +1,10 @@
-export const load = ({ params }) => {
+export async function load({ params }) {
+    const response = await fetch(`/json/${params.owner}/${params.plugin}.json`);
+    const repo_info = await response.json();
+
     return {
         owner: params.owner,
-        plugin: params.plugin
+        plugin: params.plugin,
+        repo_info: repo_info
     }
 }
