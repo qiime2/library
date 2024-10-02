3895c1a175695d4e3f69f2d15def1f154606dec9
Oddant1 <oddant1@hotmail.com>
2024-10-02 10:11:02 -0700

    Don't hard code error

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
     import "../app.css";
+
+	import { page } from '$app/stores';
 </script>
 
 <div id="container">
     <h1 class='text-4xl ml-10'>
-        404
+        {$page.status}
     </h1>
     <h1 class='text-4xl ml-10 mt-10'>
-        Not Found
+        {$page.error?.message}
     </h1>
 </div>
 
