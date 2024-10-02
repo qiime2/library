3e1805ca6150593348550d1e75aaf9a02349bef5
Oddant1 <oddant1@hotmail.com>
2024-10-02 10:01:14 -0700

    Mess with error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<div id="container">
+    <h1 class='text-4xl ml-10'>
+        404
+    </h1>
+    <h1 class='text-4xl ml-10 mt-10'>
+        File not Found
+    </h1>
+</div>
+
+<style lang="postcss">
+    #container {
+        margin-top: 70px;
+        @apply mx-auto;
+    }
+</style>
