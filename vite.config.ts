e26c973079e4c51f9c7efb02d44121aa254a0558
Evan Bolyen <evan@cym.is>
2025-02-24 16:24:08 -0700

    fix tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
-  plugins: [tailwindcss(), nodePolyfills(), sveltekit()],
+  plugins: [tailwindcss(), sveltekit()],
 });
