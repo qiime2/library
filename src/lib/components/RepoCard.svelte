dcac774766fc413e2464aad3843d4a718e74be03
Oddant1 <oddant1@hotmail.com>
2025-02-25 12:10:51 -0700

    widen the star text

--- src/lib/components/RepoCard.svelte
+++ src/lib/components/RepoCard.svelte
@@ -43,7 +43,7 @@
     </div>
     <div class="container">
         <div>
-            <svg height="24" width="40" xmlns="http://www.w3.org/2000/svg" stroke-width="1.5" stroke="currentColor">
+            <svg height="24" width="100" xmlns="http://www.w3.org/2000/svg" stroke-width="1.5" stroke="currentColor">
                 <path fill="none" stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
                 <text stroke-width="1" x="25" y="18">{repo_overview["Stars"]}</text>
             </svg>
