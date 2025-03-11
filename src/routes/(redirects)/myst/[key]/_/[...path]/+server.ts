84506cf4826b11865cc76876ee686f7903d94b26
Evan Bolyen <evan@cym.is>
2025-03-11 11:15:52 -0700

    Myst: add CORS headers to redirects

--- src/routes/(redirects)/myst/[key]/_/[...path]/+server.ts
+++ src/routes/(redirects)/myst/[key]/_/[...path]/+server.ts
@@ -3,11 +3,28 @@ import type { RequestHandler } from "./$types";
 
 import lookup from "../../../../../../../static/json/_.json";
 
+export const OPTIONS: RequestHandler = async () => {
+  return new Response(null, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, HEAD",
+    },
+  });
+};
+
 export const GET: RequestHandler = async ({ params }) => {
   const { base, pretty } = lookup[params.key as keyof typeof lookup];
   let url = base + params.path;
   if (!(pretty || url.endsWith(".json"))) {
     url += ".html";
   }
-  return redirect(307, url);
+  return new Response(null, {
+    status: 307,
+    headers: {
+      location: url,
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, HEAD",
+    },
+  });
 };
