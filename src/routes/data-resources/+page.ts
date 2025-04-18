d8b6801483af053ea3c68ab970174d0aa76ff24d
Greg Caporaso <192372+gregcaporaso@users.noreply.github.com>
2025-04-18 12:19:39 -0700

    use cloudflare url for resources site
    so we can redirect resources.qiime2.org to the library

--- src/routes/data-resources/+page.ts
+++ src/routes/data-resources/+page.ts
@@ -1,7 +1,7 @@
 import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async ({ fetch }) => {
-  const baseurl = "https://resources.qiime2.org";
+  const baseurl = "https://resources-dtz.pages.dev";
   return {
     baseurl,
     data: await fetch(`${baseurl}/index.json`).then((request) =>
