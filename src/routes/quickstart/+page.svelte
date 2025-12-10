8558a12e583d32af83ce5971fe99674a96fbfc15
Greg Caporaso <jgcap@fastmail.com>
2025-12-10 14:11:02 -0700

    update language to reflect Q2F to rachis name change

--- src/routes/quickstart/+page.svelte
+++ src/routes/quickstart/+page.svelte
@@ -6,12 +6,11 @@
 <div class="max-width">
     <article>
         <header class="prose prose-sm md:prose-base lg:prose-lg mt-10 max-w-4xl">
-            <h1>Getting Started with QIIME 2</h1>
+            <h1>Installation</h1>
             <p>
-                QIIME 2 is distributed as a combination of domain/omic-specific
-                <i>base distributions</i> and any additional plugins you may wish to install.
-                Not all plugins are compatible (or necessarily useful) with all distributions, so compatibility with
-                a base distribution is always indicated. The following base distributions are available:
+                The pages linked below provide conda and docker-based installation instructions for our base distributions, as well as the list of plugins built into each distribution, and links to relevant documentation. 
+                Other plugins can be installed in the resulting environments by following the instructions on the plugin pages. 
+                Plugin pages will provide information on which distribution(s) they are compatible with. 
             </p>
         </header>
         <div class="not-prose flex flex-wrap gap-6">
@@ -20,5 +19,5 @@
             {/each}
         </div>
     </article>
-    <div class='pb-10'><p>(Select one the of cards above to learn more.)</p></div>
+    <div class='pb-10'><p>(Select one the of cards above for installation instructions and more.)</p></div>
 </div>
